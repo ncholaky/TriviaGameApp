c7cea63e36a721c3ab158e2185bcc9eb4a38dcac
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-03 23:45:14 +0200

    Added the Views Group folder with ContentView inside it, that will be the welcome screen

--- TriviaGameApp/Components/PrimaryButton.swift
+++ TriviaGameApp/Components/PrimaryButton.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct PrimaryButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(text)
+            .foregroundColor(.white)
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 
-#Preview {
-    PrimaryButton()
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View{
+        PrimaryButton(text: "Next")
+    }
+    
 }
+
---
+++ TriviaGameApp/Views/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  TriviaGameApp
+//
+//  Created by Nicoletta Cholaky on 03/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack(spacing: 40) {
+            
+            VStack(spacing: 20) {
+                Text("Trivia Game")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color("AccentColor"))
+                
+                Text("Are you ready to test your knowledge?")
+                    .foregroundColor(Color("AccentColor"))
+                
+            }
+            PrimaryButton(text:"Let's go!")
+            
+        }
+        .frame(maxWidth:.infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .background(Color(red: 0.984313725490196, green:0.9294117647058824, blue: 0.8470588235294118))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View{
+       ContentView()
+    }
+    
+}
