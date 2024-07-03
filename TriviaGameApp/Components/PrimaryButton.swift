4a7ef4bdb71378bfb42becf0d9a722d2327de95e
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-03 23:36:08 +0200

    Created Components group folder with PrimaryButton on it

---
+++ TriviaGameApp/Components/PrimaryButton.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryButton.swift
+//  TriviaGameApp
+//
+//  Created by Nicoletta Cholaky on 03/07/24.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryButton()
+}
