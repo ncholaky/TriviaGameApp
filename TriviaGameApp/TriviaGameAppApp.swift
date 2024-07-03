b0207d704877f49b7c25988808753548597da32c
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-03 23:10:30 +0200

    Initial Commit

---
+++ TriviaGameApp/TriviaGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameAppApp.swift
+//  TriviaGameApp
+//
+//  Created by Nicoletta Cholaky on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
