7caa88f79b1c39193acd12f4e1273f6c70a7bb27
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-04 11:50:37 +0200

    We created TriviaView and embedded the outer VStack of ContentView in a NavigationView

--- TriviaGameApp/Views/ContentView.swift
+++ TriviaGameApp/Views/ContentView.swift
@@ -10,22 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack(spacing: 40) {
-            
-            VStack(spacing: 20) {
-                Text("Trivia Game")
-                    .lilacTitle()
+       NavigationView{
+            VStack(spacing: 40) {
                 
-                Text("Are you ready to test your knowledge?")
-                    .foregroundColor(Color("AccentColor"))
+                VStack(spacing: 20) {
+                    Text("Trivia Game")
+                        .lilacTitle()
+                    
+                    Text("Are you ready to test your knowledge?")
+                        .foregroundColor(Color("AccentColor"))
+                    
+                }
+                PrimaryButton(text:"Let's go!")
                 
             }
-            PrimaryButton(text:"Let's go!")
-            
-        }
-        .frame(maxWidth:.infinity, maxHeight: .infinity)
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .frame(maxWidth:.infinity, maxHeight: .infinity)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         .background(Color(red: 0.984313725490196, green:0.9294117647058824, blue: 0.8470588235294118))
+        }
     }
 }
 
---
+++ TriviaGameApp/Views/TriviaView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaView.swift
+//  TriviaGameApp
+//
+//  Created by Nicoletta Cholaky on 04/07/24.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+#Preview {
+    TriviaView()
+}
