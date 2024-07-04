06dd1834efd2cbb60c2885301f8b837df50b27e1
Nicoletta <nicoletta.cholaky@gmail.com>
2024-07-04 11:16:37 +0200

    Stage all . commit

--- TriviaGameApp/Components/AnswerRow.swift
+++ TriviaGameApp/Components/AnswerRow.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct AnswerRow: View {
+    var answer: Answer
+    @State private var isSelected = false
+    
+    var green = Color(hue: 0.437, saturation: 0.711, brightness: 0.711)
+    
+    var red = Color(red: 0.71, green: 0.894, blue: 0.1)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack (spacing: 20){
+            Image(systemName: "circle.fill")
+                .font(.caption)
+            
+            Text(answer.text)
+                .bold()
+            
+            if isSelected {
+                Spacer()
+                
+                Image(systemName: answer.isCorrect ? "checkmark.circle.fill" : "x.circle.fill")
+                    .foregroundColor(answer.isCorrect ? green: red)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundColor(isSelected ? Color("AccentColor"): .gray)
+        .background(.white)
+        .cornerRadius(10)
+        .shadow(color: isSelected ? (answer.isCorrect ? green : red) : .gray, radius: 5, x: 0.5, y: 0.5)
+        
+        
+        .onTapGesture {
+            isSelected = true
+        }
     }
 }
 
-#Preview {
-    AnswerRow()
+struct AnswerRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerRow(answer: Answer(text: "Single", isCorrect: false))
+    }
 }
